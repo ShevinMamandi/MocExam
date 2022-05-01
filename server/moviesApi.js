c3c77345a408cb05c817bcd43026ef033b7f7e3c
Shevin Mamandi <shevin1987@gmail.com>
2022-05-01 13:56:47 +0200

    connect server to mongo db

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,17 +1,9 @@
 import {Router} from "express";
 
-const movies = [
-    {
-        title: "Moviev 1"
-    },
-    {
-        title: "Moviev 2"
-    }
-];
-
-export function MoviesApi() {
+export function MoviesApi(mongoDatabase) {
     const router = new Router();
-    router.get("/", (req, res) => {
+    router.get("/", async (req, res) => {
+      const movies =   await mongoDatabase.collection("movies").find().toArray();
         res.json(movies);
     });
 
