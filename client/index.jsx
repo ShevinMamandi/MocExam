9b07115601f898985ba18acfaa37684300779954
Shevin Mamandi <shevin1987@gmail.com>
2022-05-01 10:28:40 +0200

    fixed router

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello my world!!!</h1>, document.getElementById("app"));
+function FrontPage() {
+    return(
+        <div>
+            <h1>Movies Database</h1>
+            <ul>
+                <li>
+                    <Link to={"/movies"}>List movies</Link>
+                </li>
+                <li>
+                    <Link to={"/movies/new"}>Add new movies</Link>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+function ListMovies() {
+    return (
+        <div>
+            <h1>Movies in the database</h1>
+        </div>
+    );
+}
+
+
+function AddMovies() {
+    return (
+        <form>
+            <h1>Add new movie</h1>
+        </form>
+    );
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Routes>
+            <Route path={"/"} element={<FrontPage />}/>
+            <Route path={"/movies"} element={<ListMovies />}/>
+            <Route path={"/movies/new"} element={<AddMovies />}/>
+        </Routes>
+    </BrowserRouter>;
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
