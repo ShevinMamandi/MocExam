0cf850d5f90e289716e1ed7cc9a8f8ea81c06fbc
Shevin Mamandi <shevin1987@gmail.com>
2022-05-01 14:11:26 +0200

    heroku check

--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,14 +1,16 @@
-import {Router} from "express";
+import { Router } from "express";
 
 export function MoviesApi(mongoDatabase) {
     const router = new Router();
+
     router.get("/", async (req, res) => {
-      const movies =   await mongoDatabase.collection("movies").find().toArray();
+        const movies = await mongoDatabase.collection("movies").find().toArray();
         res.json(movies);
     });
 
     router.post("/new", (req, res) => {
         res.sendStatus(500);
     });
+
     return router;
 }
--- server/server.js
+++ server/server.js
@@ -1,29 +1,29 @@
 import express from "express";
-import * as path from "path"
-import {MoviesApi} from "./moviesApi.js";
-import {MongoClient} from "mongodb";
-import dotenv from "dotenv"
+import * as path from "path";
+import { MoviesApi } from "./moviesApi.js";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-const mongoClient =   new MongoClient(process.env.MONGODB_URL);
-mongoClient.connect().then(async ()=>{
-    console.log("connected to mongodb");
-    app.use("/api/movies",MoviesApi(mongoClient.db("movie-db")));
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
+mongoClient.connect().then(async () => {
+    console.log("Connected to mongodb");
+    app.use("/api/movies", MoviesApi(mongoClient.db("movie-db")));
 });
 
 app.use(express.static("../client/dist/"));
 
-app.use((req, res, next) =>{
-    if(req.method === "GET" && !req.path.startsWith("/api")){
-        return res.sendFile(path.resolve("../client/dist/index.html"));
-    }
-    else{
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
         next();
     }
 });
+
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Started on http://localhost:${server.address().port}`);
 });
