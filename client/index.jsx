94b6f93c9e63c610d41782de0a9ba4c3988168b9
Shevin Mamandi <shevin1987@gmail.com>
2022-05-03 01:05:50 +0200

    error handling and putting values into context

--- client/index.jsx
+++ client/index.jsx
@@ -1,213 +1,230 @@
-import React, {useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import {
-    BrowserRouter,
-    Link,
-    Route,
-    Routes,
-    useNavigate,
+  BrowserRouter,
+  Link,
+  Route,
+  Routes,
+  useNavigate,
 } from "react-router-dom";
 
 import { fetchJSON } from "./http";
 import { useLoader } from "./useLoader";
 
 function FrontPage() {
-    return (
-        <div>
-            <h1>Login</h1>
-            <ul>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Login</h1>
+      <ul>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 function ManageMovies() {
-    const {loading, data, error} = useLoader(async () => {
-        return await fetchJSONData("/api/login");
-    })
-    if(loading){
-        return <div>Please wait...</div>;
-    }
-    if(error){
-        return <div>Error! {error.toString()}</div>
-    }
+  const { loading, data, error } = useLoader(async () => {
+    return await fetchJSONData("/api/login");
+  });
+  if (loading) {
+    return <div>Please wait...</div>;
+  }
+  if (error) {
+    return <div>Error! {error.toString()}</div>;
+  }
 
-    return (
-        <div>
-            <div>Profile for: {data.name}</div><br/>
-            <h1>Movie Database</h1>
-            <ul>
-                <li>
-                    <Link to={"/movies/list"}>List movies</Link>
-                </li>
-                <li>
-                    <Link to={"/movies/new"}>Add new movie</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <div>Profile for: {data.name}</div>
+      <br />
+      <h1>Movie Database</h1>
+      <ul>
+        <li>
+          <Link to={"/movies/list"}>List movies</Link>
+        </li>
+        <li>
+          <Link to={"/movies/new"}>Add new movie</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 function MovieCard({ movie: { title, plot, year } }) {
-    return (
-        <>
-            <h3>{title}</h3>
-            <h4>{year}</h4>
-            <div>{plot}</div>
-        </>
-    );
+  return (
+    <>
+      <h3>{title}</h3>
+      <h4>{year}</h4>
+      <div>{plot}</div>
+    </>
+  );
 }
 
 function ListMovies() {
-    const { loading, error, data } = useLoader(async () => {
-        return fetchJSONData("/api/movies");
-    });
+  const { loading, error, data } = useLoader(async () => {
+    return fetchJSONData("/api/movies");
+  });
 
-    if (loading) {
-        return <div>Loading.... </div>;
-    }
-    if (error) {
-        return (
-            <div>
-                <h1>Error</h1>
-                <div>{error.toString()}</div>
-            </div>
-        );
-    }
-
+  if (loading) {
+    return <div>Loading.... </div>;
+  }
+  if (error) {
     return (
-        <div>
-            <h1>List movies</h1>
-            {data.map((movie) => (
-                <MovieCard key={movie.title} movie={movie} />
-            ))}
-        </div>
+      <div>
+        <h1>Error</h1>
+        <div>{error.toString()}</div>
+      </div>
     );
+  }
+
+  return (
+    <div>
+      <h1>List movies</h1>
+      {data.map((movie) => (
+        <MovieCard key={movie.title} movie={movie} />
+      ))}
+    </div>
+  );
 }
 
 function AddMovie() {
-    const [title, setTitle] = useState("");
-    const [plot, setPlot] = useState("");
-    const [year, setYear] = useState("");
-    const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [plot, setPlot] = useState("");
+  const [year, setYear] = useState("");
+  const navigate = useNavigate();
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        await fetchJSON("/api/movies", {
-            method: "post",
-            json: { title, year, plot },
-        });
-        setTitle("");
-        setYear("");
-        setPlot("");
-        navigate("/movies/list");
-    }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    await fetchJSON("/api/movies", {
+      method: "post",
+      json: { title, year, plot },
+    });
+    setTitle("");
+    setYear("");
+    setPlot("");
+    navigate("/movies/list");
+  }
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h1>Add Movie</h1>
-            <div>
-                Title:
-                <input value={title} onChange={(e) => setTitle(e.target.value)} />
-            </div>
-            <div>
-                Year:
-                <input value={year} onChange={(e) => setYear(e.target.value)} />
-            </div>
-            <div>Plot:</div>
-            <div>
-                <textarea value={plot} onChange={(e) => setPlot(e.target.value)} />
-            </div>
-            <button>Submit</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Add Movie</h1>
+      <div>
+        Title:
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      </div>
+      <div>
+        Year:
+        <input value={year} onChange={(e) => setYear(e.target.value)} />
+      </div>
+      <div>Plot:</div>
+      <div>
+        <textarea value={plot} onChange={(e) => setPlot(e.target.value)} />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 function Movies() {
-    return (
-        <Routes>
-            <Route path={"/list"} element={<ListMovies />} />
-            <Route path={"/new"} element={<AddMovie />} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path={"/list"} element={<ListMovies />} />
+      <Route path={"/new"} element={<AddMovie />} />
+    </Routes>
+  );
 }
 
-
 async function fetchJSONData(url) {
-    const res = await fetch(url);
-    if (!res.ok) {
-        throw new Error(`Failed ${res.status}`);
-    }
-    return await res.json();
-
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+  return await res.json();
 }
 
 function Login() {
+  const { discovery_endpoint, client_id, response_type } =
+    useContext(LoginContext);
+  useEffect(async () => {
+    const { authorization_endpoint } = await fetchJSONData(discovery_endpoint);
 
-    useEffect(async () => {
-        const { authorization_endpoint } = await fetchJSONData(
-            "https://accounts.google.com/.well-known/openid-configuration"
-        );
+    const parameters = {
+      response_type,
+      client_id,
+      scope: "email profile",
+      redirect_uri: window.location.origin + "/login/callback",
+    };
 
-        const parameters = {
-            response_type: "token",
-            client_id:
-                "937896651150-boiuk03ppguqobl7v37rjulofn6tejve.apps.googleusercontent.com",
-            scope: "email profile",
-            redirect_uri: window.location.origin + "/login/callback",
-        };
+    window.location.href =
+      authorization_endpoint + "?" + new URLSearchParams(parameters);
+  }, []);
 
-        window.location.href =
-            authorization_endpoint + "?" + new URLSearchParams(parameters);
-    }, []);
-
-    return (
-        <div>
-            <h1>Please wait....</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Please wait....</h1>
+    </div>
+  );
 }
-
 
 function LoginCallback() {
-
-    const navigate = useNavigate();
-    useEffect(async () => {
-        const { access_token } = Object.fromEntries(
-            new URLSearchParams(window.location.hash.substring(1))
-        );
-        console.log(access_token);
-
-        await fetch("/api/login", {
-            method: "POST",
-            headers: {
-                "content-type": "application/json",
-            },
-            body: JSON.stringify({ access_token }),
-        });
-        navigate("/manageMovies");
-    });
-
-    return <h1>Please wait...</h1>;
-}
-
-function Application() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<FrontPage />} />
-                <Route path={"/login"} element={<Login />} />
-                <Route path={"/login/callback"} element={<LoginCallback />} />
-                <Route path={"/manageMovies"} element={<ManageMovies />} />
-                <Route path={"/movies/*"} element={<Movies />} />
-            </Routes>
-        </BrowserRouter>
+  const [error, setError] = useState();
+  const navigate = useNavigate();
+  useEffect(async () => {
+    const { access_token } = Object.fromEntries(
+      new URLSearchParams(window.location.hash.substring(1))
     );
+    console.log(access_token);
+
+    const res = await fetch("/api/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ access_token }),
+    });
+    if (res.ok) {
+      navigate("/manageMovies");
+    } else {
+      setError(`Failed POST /api/login: ${res.status} ${res.statusText}`);
+    }
+  });
+
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <div>{error}</div>
+      </div>
+    );
+  }
+  return <h1>Please wait...</h1>;
 }
 
+const LoginContext = React.createContext();
+function Application() {
+  const { loading, error, data } = useLoader(() => fetchJSON("/api/config"));
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error.toString()}</div>;
+  }
 
+  return (
+    <LoginContext.Provider value={data}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<FrontPage />} />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/login/callback"} element={<LoginCallback />} />
+          <Route path={"/manageMovies"} element={<ManageMovies />} />
+          <Route path={"/movies/*"} element={<Movies />} />
+        </Routes>
+      </BrowserRouter>
+    </LoginContext.Provider>
+  );
+}
 
 ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -15,57 +15,69 @@ app.use(bodyParser.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 async function fetchJSON(url, options) {
-    const res = await fetch(url, options);
-    if (!res.ok) {
-        throw new Error(`Failed ${res.status}`);
-    }
-    return await res.json();
+  const res = await fetch(url, options);
+  if (!res.ok) {
+    throw new Error(`Failed ${res.status}`);
+  }
+  return await res.json();
 }
+const discovery_endpoint =
+  "https://accounts.google.com/.well-known/openid-configuration";
+app.get("/api/config", (req, res) => {
+  res.json({
+    response_type: "token",
+    client_id:
+      "937896651150-boiuk03ppguqobl7v37rjulofn6tejve.apps.googleusercontent.com",
+    discovery_endpoint,
+  });
+});
 
 app.get("/api/login", async (req, res) => {
-    const { access_token } = req.signedCookies;
+  const { access_token } = req.signedCookies;
 
-    const { userinfo_endpoint } = await fetchJSON(
-        "https://accounts.google.com/.well-known/openid-configuration"
-    );
-    const userinfo = await fetchJSON(userinfo_endpoint, {
-        headers: {
-            Authorization: `Bearer ${access_token}`,
-        },
-    });
-
-    res.json(userinfo);
+  const { userinfo_endpoint } = await fetchJSON(discovery_endpoint);
+  const userinfo = await fetch(userinfo_endpoint, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+  if (userinfo.ok) {
+    res.json(await userinfo.json());
+  } else {
+    console.log(`Failed to fetch ${userinfo.status} ${userinfo.statusText}`);
+    res.sendStatus(500);
+  }
 });
 
 app.post("/api/login", (req, res) => {
-    const { access_token } = req.body;
-    res.cookie("access_token", access_token, { signed: true });
-    res.sendStatus(200);
+  const { access_token } = req.body;
+  res.cookie("access_token", access_token, { signed: true });
+  res.sendStatus(200);
 });
 
 const mongodburl = process.env.MONGODB_URL;
 if (mongodburl) {
-    const client = new MongoClient(mongodburl);
-    client
-        .connect()
-        .then((conn) =>
-            app.use(
-                "/api/movies",
-                MoviesApi(conn.db(process.env.MONGODB_DATABASE || "movie-reference"))
-            )
-        );
+  const client = new MongoClient(mongodburl);
+  client
+    .connect()
+    .then((conn) =>
+      app.use(
+        "/api/movies",
+        MoviesApi(conn.db(process.env.MONGODB_DATABASE || "movie-reference"))
+      )
+    );
 }
 
 app.use(express.static("../client/dist/"));
 
 app.use((req, res, next) => {
-    if (req.method === "GET" && !req.path.startsWith("/api")) {
-        res.sendFile(path.resolve("../client/dist/index.html"));
-    } else {
-        next();
-    }
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Started on http://localhost:${server.address().port}`);
+  console.log(`Started on http://localhost:${server.address().port}`);
 });
